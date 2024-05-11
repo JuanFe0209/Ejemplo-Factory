78c69457d47e22418835a6351d5fe32aadbf5c13
Juan Felipe Rojas <piperbueno@hotmail.com>
2024-05-10 21:34:46 -0500

    .

--- .idea/main.py
+++ .idea/main.py
@@ -5,7 +5,7 @@ def main():
     vehiculo1 = fabrica.fabricar_vehiculo("automovil", "Toyota", "Corolla")
     vehiculo1.conducir()
 
-    vehiculo2 = fabrica.fabricar_vehiculo("motocicleta", "Honda", "CBR600RR")
+    vehiculo2 = fabrica.fabricar_vehiculo("motocicleta", "Honda", "2016")
     vehiculo2.conducir()
 
 if __name__ == "__main__":
